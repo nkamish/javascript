b28cb60d5141a0e938fc0b18fa8320437cac4841
nkamish <natheerkamish@gmail.com>
2018-10-24 11:37:38 -0700

    Added console log

--- js/main.js
+++ js/main.js
@@ -1,12 +1,26 @@
-// Variables Listed bellow-------------------------------------------------->
+// Variables Listed bellow---------------------------------------------------------------------->
 
 var message = "Good morning";
     user = "Natheer";
     age = "22";
     gender = "Male";
+    address = "Hanover park";
 
 
-alert(message);
-alert(user);
-alert(age);
-alert(gender);
+// alert(message);
+// alert(user);
+// alert(age);
+// alert(gender);
+
+console.log('Hi my name is',  user, 'I am',  age, '. I live in',  address);
+
+// Alert which uses variables. Recalling variables used for alert-------------------------------->
+var student, admin;
+    student = "Natheer Kamish";
+    admin = student;
+
+
+// alert(student);
+alert(admin);
+
+
